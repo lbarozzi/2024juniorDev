d975b853adb7c42691afc4e23330cd163389c651
Leonardo Barozzi <leonardo.barozzi@mail.com>
2024-02-23 10:06:17 +0100

    WepApiController

---
+++ WebDay4/WebApplication1/Models/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models {
+    public class DataContext : DbContext {
+        public DataContext(DbContextOptions<DataContext> opt) :base(opt) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
